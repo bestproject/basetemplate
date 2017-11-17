ffae8f7eed1d16140cf68ca5648cbd8decc84776
Artur Stępień <artur.stepien@bestproject.pl>
2017-11-17 14:41:27 +0100

    Component view

---
+++ component.php
@@ -0,0 +1,40 @@
+<?php defined('_JEXEC') or die;
+
+// Prepare document head
+require_once __DIR__.'/includes.php';
+use BestProject\Bootstrap3;
+
+
+/**
+ * == CUSTOM VARIABLES =========================================================
+ */
+// Enter your custom variables here
+
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $language; ?>" lang="<?php echo $language; ?>" dir="<?php echo $direction; ?>">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <jdoc:include type="head" />
+        <!--[if lt IE 9]>
+            <script src="<?php echo JUri::root(true); ?>/media/jui/js/html5.js"></script>
+        <![endif]-->
+    </head>
+
+    <body class="site <?php echo $class ?> modal-view">
+
+        <!--Main content-->
+        <main class="col-xs-12">
+
+            <jdoc:include type="component" />
+
+        </main>
+
+    </body>
+</html>
+<?php
+
+// Remove Bootstrap 2
+$document = JFactory::getDocument();
+unset($document->_scripts['/media/jui/js/bootstrap.min.js']);
+unset($document->_scripts['/media/jui/js/bootstrap.js']);
