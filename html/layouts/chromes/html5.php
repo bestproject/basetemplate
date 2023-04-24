de3e69252ebb1958dc5395cc0cd01e5fd4e2ef17
Artur Stępień <artur.stepien@bestproject.pl>
2023-04-24 19:31:15 +0200

    Added module-title class to module headings in html5 module style.

--- html/layouts/chromes/html5.php
+++ html/layouts/chromes/html5.php
@@ -28,12 +28,12 @@ $moduleAttribs['class'] = 'moduletable ' . htmlspecialchars($params->get('module
 $bootstrapSize          = (int) $params->get('bootstrap_size', 0);
 $moduleAttribs['class'] .= $bootstrapSize !== 0 ? ' col-md-' . $bootstrapSize : '';
 $headerTag              = htmlspecialchars($params->get('header_tag', 'h3'), ENT_QUOTES, 'UTF-8');
-$headerClass            = htmlspecialchars($params->get('header_class', ''), ENT_QUOTES, 'UTF-8');
+$headerClass            = htmlspecialchars('module-title '.$params->get('header_class', ''), ENT_QUOTES, 'UTF-8');
 $headerAttribs          = [];
 
 // Only output a header class if one is set
 if ($headerClass !== '') {
-    $headerAttribs['class'] = $headerClass;
+    $headerAttribs['class'] = trim($headerClass);
 }
 
 // Only add aria if the moduleTag is not a div
